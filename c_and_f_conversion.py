bbd2ba4f134e75830bf3272b6965589852ee2079
Aishwarya Purandare <96563456+aishwarya0918@users.noreply.github.com>
2022-10-28 18:51:58 +0530

    Celsius to Fahrenheit and Fahrenheit to Celcius conversion

---
+++ c_and_f_conversion.py
@@ -0,0 +1,30 @@
+def ctof():
+     c =float(input("Enter value in degree Celcuis:"))
+     f= (c*1.8)+32
+     print("The corresponding value in degree Fahrenheit is:")
+     print(f)
+     return
+ 
+def ftoc():
+     f =float(input("Enter value in degree Fahrenheit:"))
+     c = ((f-32)*5)/9
+     print("The corresponding value in degree Celcius is:")
+     print(c)
+     return
+    
+def main():
+    option=0
+    while(option!=3):
+        print("\nMENU")
+        print("Enter the conversion you want to do:")
+        print("1.Celcius to Fahrenheit")
+        print("2.Fahrenheit to Celcius")
+        print("3.Exit")
+        option = int(input("Enter your choice:"))
+        if (option==1):
+            ctof()
+        if (option==2):
+            ftoc()
+        elif (option==3):
+            print("THANK YOU!")
+main()
